cfd721ca71a83d0fd838cf3c969dee2cf7113537
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-07-07 19:09:59 +0200

    Use django built in Form module to generate form

--- app/views.py
+++ app/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+class NewGameForm(forms.Form):
+    columns = forms.IntegerField(label="columns")
+    rows = forms.IntegerField(label="rows")
+    generations = forms.IntegerField(label="generations")
+
+
+grid_data = []
 
 
 # Create your views here.
